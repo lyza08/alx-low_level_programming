d72489a0edad4e74df3bcd0db177bc314643aad1
lyza08 <oppongliza18@gmail.com>
2023-04-21 14:12:17 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description -'print alphabets in lowercase except e and q'
+ * Return: Always 0 (successful)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		printf("%d", x);
+	}
+	printf("\n");
+	return (0);
+}
