7266b5f0abf39c758101bd8c99781031183cb96b
lyza08 <oppongliza18@gmail.com>
2023-04-21 14:22:30 -0700

    Still Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description -'printing single digits with putchar'
+ * Return: Always 0 (successful)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar(x + 48);
+	}
+	printf("\n");
+	return (0);
+}
