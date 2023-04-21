ab863e02555b7775f8a851eb8ece53836fe99c1a
lyza08 <oppongliza18@gmail.com>
2023-04-21 11:15:53 -0700

    Suffering from Insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - 'print lowecase alphabets'
+ * Return: always 0 (successful)
+ */
+
+int main(void)
+{
+	int x = 97;
+
+	while (x <= 122)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}        
