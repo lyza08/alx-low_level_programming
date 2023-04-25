07933715155e1dab1c99decc7f0f295c9d55aa08
lyza08 <oppongliza18@gmail.com>
2023-04-25 09:40:45 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - printing alphabets in lower case
+ *
+ * Return: void (successful)
+ */
+
+void print_alphabet(void)
+{
+	int g;
+
+	for (g = 97; g <= 122; g++)
+	{
+	_putchar(g);
+	}
+	_putchar('\n');
+}
