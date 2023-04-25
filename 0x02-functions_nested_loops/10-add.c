0fa250683dd5562233feee13605887d6fa17fc1c
lyza08 <oppongliza18@gmail.com>
2023-04-25 14:15:39 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * add - Entry point
+  * Description - 'a function that adds two integers and print'
+  * @x1: first number to be computed
+  * @x2: second number to be computed
+  * Return: integer value or results
+  */
+
+int add(int x1, int x2)
+{
+	return (x1 + x2);
+}
