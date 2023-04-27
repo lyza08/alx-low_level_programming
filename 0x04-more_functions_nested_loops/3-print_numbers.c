40d07329481f4cd2bbc2831d5230b05594c64640
lyza08 <oppongliza18@gmail.com>
2023-04-27 08:49:48 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_numbers - Entry point
+  * Description - 'printing numbers 0 - 9'
+  * Return: Always 0 (successful)
+  */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+	_putchar(x);
+	}
+	_putchar('\n');
+}
