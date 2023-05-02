19004b8045115897e53006fff821240531b1f17b
Liza Oppong <oppongliza18@gmail.com>
2023-05-02 07:54:54 -0700

    98 Battery

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - Entry point
+  * Description - taking a pointer to an int as parameter
+  * @n: Integer input pointer
+  * Return: nothing (successful)
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
