fb7515c7338d4f0042357016ccb02428ea3f3f21
Liza Oppong <oppongliza18@gmail.com>
2023-05-10 07:43:52 -0700

    Locked away

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * _print_rev_recursion - Entry point
+  * Description - function that prints a string in reverse recursively
+  * @s: string to be treated
+  * Return: void (successful)
+  */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
