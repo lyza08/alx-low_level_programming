dc54085fa3f09c3f1314698920137caaa9ab8cc8
Liza Oppong <oppongliza18@gmail.com>
2023-05-12 07:59:55 -0700

    Whats your answer

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - printing a program name
+ * @argc: And count the number of arguments
+ * @argv: array of strings
+ * Return: Always 0 (successful)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
