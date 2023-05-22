5507809178d028ba92b2eb95dbe9e726d536908f
Liza Oppong <oppongliza18@gmail.com>
2023-05-22 02:46:16 -0700

    Functions like Macros

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,19 @@
+/*
+ * file: 2-main.c
+ */
+
+
+#include <stdio.h>
+
+/**
+ * main - printing the name of the file of the program
+ *        was compiled from, followed by a new line.
+ * Return: Always 0 (successful)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
