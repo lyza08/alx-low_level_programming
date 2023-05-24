66a314cf2dd54a1e174d36857971bce66fc378e6
Liza Oppong <oppongliza18@gmail.com>
2023-05-24 03:10:41 -0700

    Fuction Pointers Project

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - entry point
+ * Description: prints each array elem on a newl
+ * @array: array
+ * @size: how many elem to print
+ * @action: pointer to print in regular or hex
+ * Return: void successful
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int x;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (x = 0; x < size; x++)
+	{
+		action(array[x]);
+	}
+}
+
+
